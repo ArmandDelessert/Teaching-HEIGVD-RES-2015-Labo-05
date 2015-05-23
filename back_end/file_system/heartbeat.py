93ab41a3c14f9847469b1223a487b214b85397ac
raphaelracine <raphael.racine@heig-vd.ch>
2015-05-23 14:56:40 +0200

    Modifications reverse proxy

--- back_end/file_system/heartbeat.py
+++ back_end/file_system/heartbeat.py
@@ -1,10 +1,10 @@
 import socket, time
 
-SERVER_IP = '127.0.0.1'; SERVER_PORT = 43278; BEAT_PERIOD = 1
+ip_destination = '255.1.1.1'; 
+port_destination = 8000; 
+BEAT_PERIOD = 1
 
-print ('Sending heartbeat to IP %s , port %d\n'
-    'press Ctrl-C to stop\n') % (SERVER_IP, SERVER_PORT)
 while True:
     hbSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    hbSocket.sendto('FRONTEND', (SERVER_IP, SERVER_PORT))
+    hbSocket.sendto('BACKEND', (SERVER_IP, SERVER_PORT))
     time.sleep(BEAT_PERIOD)
