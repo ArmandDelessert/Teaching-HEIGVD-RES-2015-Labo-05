a219d79ad06d6fdc1aa22a5ee46a9b1a3364f34e
raphaelracine <raphael.racine@heig-vd.ch>
2015-05-23 14:11:28 +0200

    Preparation des fichiers pour heartbeat FE  et   BE

--- back_end/file_system/start.sh
+++ back_end/file_system/start.sh
@@ -1,1 +1,2 @@
 pm2 start -x /back_end/bin/www --no-daemon
+pm2 start -x --interpreter=python heartbeat.py --no-daemon
--- front_end/file_system/start.sh
+++ front_end/file_system/start.sh
@@ -1,1 +1,2 @@
 pm2 start -x /front_end/bin/www --no-daemon
+pm2 start -x --interpreter=python heartbeat.py --no-daemon
