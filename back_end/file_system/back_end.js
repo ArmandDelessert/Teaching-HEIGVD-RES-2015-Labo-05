787956b45f4116a99dbad6e5ac692cd4b002261b
raphaelracine <raphael.racine@heig-vd.ch>
2015-05-25 11:41:19 +0200

    Tout fonctionne, sans heartbeat, testé

--- back_end/file_system/back_end.js
+++ back_end/file_system/back_end.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 
-var array = ['Raphaël', 'Armand', 'Olivier', 'Simon'];
+var array = ['Raphael', 'Armand', 'Olivier', 'Simon'];
 
 app.get('/', function (req, res) {
-  res.send(array[Math.floor(Math.random() * (4))]);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({"studentname":array[Math.floor(Math.random() * 4)]}));
 });
 
 var server = app.listen(80, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 });
--- front_end/www/getStudent.js
+++ front_end/www/getStudent.js
@@ -1,11 +1,11 @@
 function getStudent()
 {	
-	$.getStudent("/api", function(result)
+	$.getJSON("/api", function(result)
 	{		
 	
 		$.each(result, function(name, value)
 		{				
-			$("#student").append(name + " => " + value + "<br/>");
+			$("#student").append("Etudiant tire au sort :" + value + "<br/>");
 		});
 		
 		$("#student").show();
