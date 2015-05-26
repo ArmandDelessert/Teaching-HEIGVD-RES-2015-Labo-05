a9d035ae58fe8705a6543f399f5146ddd5ceebc0
simon-baehler <simon.baehler@heig-vd.ch>
2015-05-26 11:10:39 +0200

    ajout du HB front et end

--- back_end/HB.js
+++ back_end/HB.js
@@ -8,7 +8,7 @@ function sendInfo()
 {
 	var PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
 	var PROTOCOL_PORT = 6666;
-	var buffer = "FE ";
+	var buffer = "BE ";
 	var ip = "<?php echo $_SERVER['SERVER_ADDR']; ?>";
 	buffer = message + ip
 	var message = new Buffer(buffer);
@@ -26,7 +26,7 @@ function sendInfo()
 var heartbeats = require('heartbeats');
 var heart = heartbeats.createHeart(5000);
 
-//tout les battement nous envoyons les infos FE (pour front end) et l'ip de la machine
+//tout les battement nous envoyons les infos BE (pour back end) et l'ip de la machine
 heart.createEvent(1, function(heartbeat, last){
 	sendInfo();
 });
