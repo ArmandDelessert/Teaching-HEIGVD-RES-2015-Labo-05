3d3332f7cd6dbbea835fadcb21ba136192eaaf64
simon-baehler <simon.baehler@heig-vd.ch>
2015-05-30 14:26:43 +0200

    ajout de la doc HB coté front/back_end

--- front_end/HB.js
+++ front_end/HB.js
@@ -1,28 +1,18 @@
-
-
-
 var dgram = require('dgram');
 var s = dgram.createSocket('udp4');
 var os = require('os');
 var ifaces = os.networkInterfaces();
 var IP;
 
-
 Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0
-    ;
-
+  var alias = 0;
   ifaces[ifname].forEach(function (iface) {
     if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
     }
-
     if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
       console.log(ifname + ':' + alias, iface.address);
     } else {
-      // this interface has only one ipv4 adress
       console.log(ifname, iface.address);
     }
 	IP = iface.address;
@@ -59,7 +49,6 @@ function sendInfo(FE, IP) {
 }
 
 var FE = true;
-
 var t1 = new sendInfo(FE, IP);
 
 
