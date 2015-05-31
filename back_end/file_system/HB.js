0e2f8fce1d5192d5b1e302d3f7b7ef771a663a42
raphaelracine <raphael.racine@heig-vd.ch>
2015-05-31 14:52:10 +0200

    modif

--- back_end/file_system/HB.js
+++ back_end/file_system/HB.js
@@ -1,29 +1,65 @@
 
-//doc here : https://www.npmjs.com/package/heartbeats
-
-var heartbeats = require('heartbeats');
-var heart = heartbeats.createHeart(5000);
 
 
-function sendInfo()
-{
-	var PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
-	var PROTOCOL_PORT = 6666;
-	var buffer = "BE ";
-	var ip = "<?php echo $_SERVER['SERVER_ADDR']; ?>";
-	buffer = message + ip
-	var message = new Buffer(buffer);
+var dgram = require('dgram');
+var s = dgram.createSocket('udp4');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var IP;
 
-	var client = dgram.createSocket("udp4");
-	client.bind();
-	client.on("listening", function () {
-		client.setBroadcast(true);
-		client.send(message, 0, message.length, PROTOCOL_PORT, broadcastAddress, function(err, bytes) {
-			client.close();
-		});
-	});
-}
-//tout les battement nous envoyons les infos FE (pour back end) et l'ip de la machine
-heart.createEvent(1, function(heartbeat, last){
-	sendInfo();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0
+    ;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+	IP = iface.address;
+  });
 });
+
+
+function sendInfo(FE, IP) {
+
+	var that = this;
+
+	this.FE = FE;
+	this.IP = IP;
+		
+	
+	sendInfo.prototype.update = function() {  
+	 
+	  var info = new Object();
+	  info.FE = that.FE;
+	  var PROTOCOL_PORT = 6666;
+	  var PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
+	  info.IP = that.IP;
+	  console.log(info.FE);
+	  console.log(info.IP);
+	  var payload = JSON.stringify(info);
+	  message = new Buffer(payload);	
+	  s.send(message, 0, message.length, PROTOCOL_PORT, PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
+	  	console.log("Sending payload: " + payload + " via port " + s.address().port);
+	  });
+	
+	}
+	setInterval(that.update, 2000);
+	
+}
+
+var FE = false;
+
+var t1 = new sendInfo(FE, IP);
+
+
